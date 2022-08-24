dc3bf7addc207e900f2269783f1d629d081dffe7
bahjat.tounsi <bahjattounsi1@hotmail.com>
2022-08-24 10:00:29 +0200

    Distance working

--- js/triVille.js
+++ js/triVille.js
@@ -61,8 +61,24 @@ function getArrayCsv(csv) {
  * @returns la distance qui sépare la ville de Grenoble
  */
 function distanceFromGrenoble(ville) {
-    console.log('implement me !');
-    return 0;
+
+    const grenobleLatitude = 45.166667;
+    const grenobleLongitude = 5.716667;
+
+    const R = 6371e3; // metres
+    const φ1 = ville.latitude * Math.PI/180; // φ, λ in radians
+    const φ2 = grenobleLatitude * Math.PI/180;
+    const Δφ = (grenobleLatitude-ville.latitude) * Math.PI/180;
+    const Δλ = (grenobleLongitude-ville.longitude) * Math.PI/180;
+
+    const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    const d = R * c; // in metres
+
+    return d;
 }
 
 /**
